32ea2d3955f074d216a28fc050010b041fb88e97
Anthony NorderhendqCDcd <anthony@norderhaug.org>
2023-02-01 13:58:34 -0800

    punctuation implementation

--- Manager.py
+++ Manager.py
@@ -1,63 +1,75 @@
 import json
-import subprocess
 from threading import Thread
 from queue import Queue
-import time
 import pyaudio
+import time
+from fastpunct import FastPunct
 from vosk import Model, KaldiRecognizer
 
-messages = Queue()          # Tracks when recording is desired - thread communication
-recordings = Queue()        # Holds all recording files in 20 sec increments
+messages = Queue()  # Tracks when recording is desired - thread communication
+recordings = []
 
 # Microphone recording
-CHANNELS = 1                # Speech recognition works best w/ 1 channel
+CHANNELS = 1  # Speech recognition works best w/ 1 channel
 FRAME_RATE = 16000
 RECORD_SECONDS = 5
 AUDIO_FORMAT = pyaudio.paInt16
 SAMPLE_SIZE = 2
 
 # Speech recognition
-model = Model(model_name="vosk-model-en-us-0.22")   # Creating model, downloads it if not present
+# -> Loads model's data, downloads model into cache if not present
+model = Model(model_name="vosk-model-en-us-0.22")
 rec = KaldiRecognizer(model, FRAME_RATE)
-rec.SetWords(True)                                  # Confidence probabilities
+fastpunct = FastPunct()
+
+rec.SetWords(True)  # Confidence probabilities
+
+
+def timer():
+    print("Starting timer!")
+    secs = 0
+
+    while True:
+        time.sleep(1)
+        secs += 1
+
+        if messages.empty():  # More accuracy within thread for timer
+            break
+        else:
+            print(f"{secs} seconds...")
 
 
 def start_recording():
-    messages.put(True, block=True)      # Ping for recording to initiate
+    messages.put(True)  # Ping for recording to initiate
 
     # Begin recording
     print("Starting...")
-    record = Thread(target=record_microphone)
+
+    # stopwatch = Thread(target=timer)  # Timer
+    # stopwatch.start()
+
+    record = Thread(target=record_microphone)  # Record microphone
     record.start()
 
-    # Begin transcription
-    transcribe = Thread(target=speech_recognition)
-    transcribe.start()
 
 def stop_recording():
-    messages.get()          # Ping for recording to stop
+    messages.get()  # Ping for recording to stop
     print("Stopped.")
 
 
-def record_microphone(chunk=1024):          # Reading 1024 frames at a time
+def record_microphone(chunk=1024):  # Reading 1024 frames at a time
     p = pyaudio.PyAudio()
 
     stream = p.open(format=AUDIO_FORMAT,
                     channels=CHANNELS,
                     rate=FRAME_RATE,
                     input=True,
-                    input_device_index=3,   # Arctic Steel Series 7 - Chat index
+                    input_device_index=6,  # Input device, use 'Devices.py' to utilize it
                     frames_per_buffer=chunk)
-    frames = []
 
-    while not messages.empty():     # Recording is ongoing
-        data = stream.read(chunk)   # Read & store 'chunk' sized data from stream
-        frames.append(data)
-
-        # Once 20 secs of audio recorded
-        if len(frames) >= (FRAME_RATE * RECORD_SECONDS) / chunk:
-            recordings.put(frames.copy())
-            frames = []
+    while not messages.empty():  # Recording is ongoing
+        data = stream.read(chunk)  # Read & store 'chunk' sized data from stream
+        recordings.append(data)
 
     # Closing stream & PyAudio
     stream.stop_stream()
@@ -65,23 +77,30 @@ def record_microphone(chunk=1024):          # Reading 1024 frames at a time
     p.terminate()
 
 
-def speech_recognition():
-    while not messages.empty():     # Recording desired
-        frames = recordings.get()   # Pulling saved frames
+def speech_recognition(chunks):
+    rec.AcceptWaveform(b''.join(chunks))  # Joining frames into binary string
 
-        rec.AcceptWaveform(b''.join(frames)) # Joining frames into binary string
-        result = rec.Result()
-        text = json.loads(result)["text"]
+    result = rec.Result()  # Result, VOSK returns JSON object
+    text = json.loads(result)["text"]
 
-        # Running shell command "repunc", applying punctuation to text
-        # Can be optimized by integrating recasepunc.py
-        print(text)
+    print(f"VOSK:\n {text}")
+    correct_punctuation(text)
+
+
+def correct_punctuation(text):
+    data = fastpunct.punct(text)
+    result = ""
+
+    for fragment in data:
+        result += fragment
+
+    print(f"VOSK + FastPunct:\n {result}")
 
 
 if __name__ == '__main__':
     start_recording()
 
-    print("Press ENTER to stop recording.")
-    input()
-
+    input("Press ENTER to stop recording.")
     stop_recording()
+
+    speech_recognition(recordings)
